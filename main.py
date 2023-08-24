4edc6ac04cd637db9f1a7dcf7d50d9236d386e9f
Thibesan Raveenderan <traveen2@uwo.ca>
2023-08-23 20:20:08 -0400

    Annotations, Function Toggle
    Finalized logic for recognition function, included annotations for facial bounds.
    
    Functions as intended, need to bugfix slow performance in video feed.

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class FaceRecognition:
         
         print(self.knownFaceNames)
 
-        def runRecognition(self):
+    def runRecognition(self):
             videoCapture = cv2.VideoCapture(0)
 
             if not videoCapture.isOpened():
@@ -52,8 +52,8 @@ class FaceRecognition:
                     smallFrame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
                     rgbSmallFrame = cv2.cvtColor(smallFrame, cv2.COLOR_BGR2RGB) #Change Color params for 1:4 scale resized frame
 
-                    self.faceLocations = face_recognition.faceLocations(rgbSmallFrame)
-                    self.faceEncodings = face_recognition.faceEncodings(rgbSmallFrame, self.faceLocations)
+                    self.faceLocations = face_recognition.face_locations(rgbSmallFrame)
+                    self.faceEncodings = face_recognition.face_encodings(rgbSmallFrame, self.faceLocations)
 
                     self.faceNames = []
                     for faceEncoding in self.faceEncodings:
@@ -70,9 +70,32 @@ class FaceRecognition:
 
                         self.faceNames.append(f'{name} ({confidence})')
 
+                #Toggle Recognition Function, Preserve Resources
                 self.processCurrentFrame = not self.processCurrentFrame
+
+                #Display Function Info as Annotation
+                for (top, right, bottom, left), name in zip(self.faceLocations, self.faceNames):
+                    top *= 4
+                    right *= 4
+                    bottom *= 4
+                    left *= 4
+
+                    #Rectangle Outline of Face Location
+                    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                    #Confidence Level Display in Outline
+                    cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), -1)
+                    cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+
+                cv2.imshow('Face Recognition', frame)
+
+                if cv2.waitKey(1) == ord('x'):
+                    break
+
+            videoCapture.release()
+            cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     fr = FaceRecognition()
+    fr.runRecognition()
 
 
